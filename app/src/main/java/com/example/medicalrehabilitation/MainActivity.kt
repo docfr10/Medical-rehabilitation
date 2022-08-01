8b1fc94456e702e691e8c9d36dc6f1663c9416a4
docfr10 <docfr10@yandex.ru>
2022-08-01 12:11:26 +0300

    Added a training screen template

--- app/src/main/java/com/example/medicalrehabilitation/MainActivity.kt
+++ app/src/main/java/com/example/medicalrehabilitation/MainActivity.kt
@@ -11,14 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startbutton: ImageButton = findViewById(R.id.begin_button)
-        val aboutbutton: Button = findViewById(R.id.button_about)
+        val startbutton: ImageButton = findViewById(R.id.begin_imagebutton)
+        val aboutbutton: Button = findViewById(R.id.about_button)
 
+        //Переход с помощью кнопки к тренировке
         startbutton.setOnClickListener {
             val intent = Intent(this@MainActivity, Training::class.java)
             startActivity(intent)
         }
 
+        //Переход с помощью кнопки к информации о приложении
         aboutbutton.setOnClickListener {
             val intent = Intent(this@MainActivity, About::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/medicalrehabilitation/Training.kt
+++ app/src/main/java/com/example/medicalrehabilitation/Training.kt
@@ -1,11 +1,22 @@
 package com.example.medicalrehabilitation
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 
 class Training : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
+
+        val videoView: VideoView = findViewById(R.id.videoView)
+        val myVideoUri: Uri = Uri.parse("android.resource://$packageName/" + R.raw.video)
+        videoView.setVideoURI(myVideoUri)
+
+        val mediaController: MediaController = MediaController(this)
+        videoView.setMediaController(mediaController)
+        mediaController.setAnchorView(videoView)
     }
 }
