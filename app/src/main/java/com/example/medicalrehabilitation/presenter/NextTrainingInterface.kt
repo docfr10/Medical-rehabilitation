20dafa3c05a5122386eb04760a6aac239d83f884
docfr10 <docfr10@yandex.ru>
2022-08-15 10:14:12 +0300

    Added NextTrainingPresenter

---
+++ app/src/main/java/com/example/medicalrehabilitation/presenter/NextTrainingInterface.kt
@@ -0,0 +1,37 @@
+package com.example.medicalrehabilitation.presenter
+
+import android.app.AlarmManager
+import android.app.AlertDialog
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.example.medicalrehabilitation.databinding.ActivityNextTrainingBinding
+import java.text.DateFormat
+import java.util.*
+
+interface NextTrainingInterface {
+    fun createNotifications(
+        intent: Intent,
+        applicationContext: Context,
+        alarmManager: AlarmManager,
+        time: Long
+    )
+
+    fun getTime(binding: ActivityNextTrainingBinding): Long
+    fun createNotificationChannel(
+        binding: ActivityNextTrainingBinding,
+        notificationManager: NotificationManager
+    )
+
+    fun showAlert(
+        time: Long,
+        date: Date,
+        dateFormat: DateFormat,
+        timeFormat: DateFormat,
+        intent1: Intent,
+        applicationContext: Context,
+        savedInstanceState: Bundle?,
+        builder: AlertDialog.Builder
+    )
+}
--- app/src/main/java/com/example/medicalrehabilitation/view/NextTraining.kt
+++ app/src/main/java/com/example/medicalrehabilitation/view/NextTraining.kt
@@ -2,119 +2,61 @@ package com.example.medicalrehabilitation.view
 
 import android.app.*
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.example.medicalrehabilitation.*
+import com.example.medicalrehabilitation.Notifications
 import com.example.medicalrehabilitation.databinding.ActivityNextTrainingBinding
+import com.example.medicalrehabilitation.presenter.NextTrainingPresenter
 import java.util.*
 
 //Класс, отвечающий за выбор даты следующей тренировки
 class NextTraining : AppCompatActivity() {
 
-    private lateinit var binding: ActivityNextTrainingBinding //Библиотека binding
-                                                              // упрощает работу с компонентами GUI
+    private lateinit var binding: ActivityNextTrainingBinding //Библиотека binding упрощает работу с компонентами GUI
+    private var nextTrainingPresenter: NextTrainingPresenter = NextTrainingPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Убирается панель действий сверху
-        window.requestFeature(Window.FEATURE_NO_TITLE)
-        supportActionBar?.hide()
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         binding = ActivityNextTrainingBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-
         createNotificationChannel()
         //С помощью binding обрабатываем нажатия на кнопку
-        binding.button.setOnClickListener { createNotifications() }
+        binding.button.setOnClickListener { createNotifications(savedInstanceState) }
     }
 
-    //Метод, отвечающий за создание уведомлений на панели действий
-    private fun createNotifications() {
+    private fun createNotificationChannel() {
+        val notificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        nextTrainingPresenter.createNotificationChannel(binding, notificationManager)
+    }
+
+    private fun createNotifications(savedInstanceState: Bundle?) {
         val intent = Intent(applicationContext, Notifications::class.java)
-        val title = this.getString(R.string.notification) //Название уведомления
-        val message = this.getString(R.string.training_time) //Текст уведомления
-        //Передаем название и текст уведомления в Notifications
-        intent.putExtra(titleExtra, title)
-        intent.putExtra(messageExtra, message)
-
-        //Создаем широковещательный сигнал для отправки уведомления
-        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            PendingIntent.getBroadcast(
-                applicationContext,
-                notificationID,
-                intent,
-                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-            )
-        } else {
-            TODO("VERSION.SDK_INT < M")
-        }
-
         //Записываем дату когда необходимо отправить уведомление
         val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-        val time = getTime()
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            time,
-            pendingIntent
-        )
-        showAlert(time)
+        val time = nextTrainingPresenter.getTime(binding)
+        nextTrainingPresenter.createNotifications(intent, applicationContext, alarmManager, time)
+        showAlert(savedInstanceState)
     }
 
-    //Метод, отвечающий за показ пользователю того когда будет отправлено уведомление
-    private fun showAlert(time: Long) {
+    private fun showAlert(savedInstanceState: Bundle?) {
+        val time = nextTrainingPresenter.getTime(binding)
         val date = Date(time)
         val dateFormat = android.text.format.DateFormat.getLongDateFormat(applicationContext)
         val timeFormat = android.text.format.DateFormat.getTimeFormat(applicationContext)
         val intent1 = Intent(this@NextTraining, SendMail::class.java)
-
-        AlertDialog.Builder(this)
-            .setTitle(R.string.notification_scheduled)
-            .setMessage(
-                dateFormat.format(date) + " " + timeFormat.format(date)
-            )
-            .setPositiveButton(R.string.clear) { _, _ -> startActivity(intent1) }
-            .show()
-    }
-
-    //Метод, переводящий выбранные значения даты и времени в миллисекунды
-    private fun getTime(): Long {
-        val minute = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            binding.timePicker.minute
-        } else {
-            TODO("VERSION.SDK_INT < M")
-        }
-        val hour = binding.timePicker.hour
-        val day = binding.datePicker.dayOfMonth
-        val month = binding.datePicker.month
-        val year = binding.datePicker.year
-
-        val calendar = Calendar.getInstance()
-        calendar.set(year, month, day, hour, minute)
-        //Возвращает миллисекунды чтобы через время отправить уведомление
-        return calendar.timeInMillis
-    }
-
-    //Метод, отвечающий за создание канала уведомлений
-    private fun createNotificationChannel() {
-        val name = "Notification Channel"
-        val desc = "A Description of the Channel"
-        val importance = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            NotificationManager.IMPORTANCE_DEFAULT
-        } else {
-            TODO("VERSION.SDK_INT < N")
-        }
-        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(channelID, name, importance)
-        } else {
-            TODO("VERSION.SDK_INT < O")
-        }
-        channel.description = desc
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+        val builder = AlertDialog.Builder(this)
+        nextTrainingPresenter.showAlert(
+            time,
+            date,
+            dateFormat,
+            timeFormat,
+            intent1,
+            applicationContext,
+            savedInstanceState,
+            builder
+        )
+        startActivity(intent1)
     }
 }
