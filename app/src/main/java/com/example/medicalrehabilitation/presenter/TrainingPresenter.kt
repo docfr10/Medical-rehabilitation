d11c1da7d647db20dd69b324f364a9cf155578b9
docfr10 <docfr10@yandex.ru>
2022-08-19 19:40:59 +0300

    Added new descriptions

--- app/src/main/java/com/example/medicalrehabilitation/presenter/TrainingPresenter.kt
+++ app/src/main/java/com/example/medicalrehabilitation/presenter/TrainingPresenter.kt
@@ -105,8 +105,8 @@ class TrainingPresenter : TrainingInterface {
     //Смена информации об упражнении, реализована в виде диалогового окна
     override fun aboutExercise(builder: AlertDialog.Builder) {
         when (numberOfTraining) {
-            1 -> builder.setMessage(R.string.description1)
-            2 -> builder.setMessage("Описание второго упражнения")
+            1 -> builder.setMessage(R.string.exercise1)
+            2,3,4 -> builder.setMessage(R.string.exercise2_3_4)
         }
         builder.setTitle((R.string.about_exercise))
             .setPositiveButton((R.string.clear)) { dialog, _ -> dialog.cancel() }
